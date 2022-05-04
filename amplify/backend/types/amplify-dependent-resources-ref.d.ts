64a796914978e9ca1229b6cbef29fc7ba9a8c12f
rjm13 <65667098+rjm13@users.noreply.github.com>
2022-05-04 13:43:18 -0500

    amplify initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "blipdev587bb707": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "blipdev": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3671a5d42": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
